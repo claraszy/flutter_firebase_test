7000c09172f6dd80638ef1e0b60270030c8c5fe3
Tomas Navas <tomasnavasgutierrez@gmail.com>
2019-11-23 19:21:27 +0100

    Security change

--- lib/main.dart
+++ lib/main.dart
@@ -13,138 +13,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: Home(),
+      home: new AppHome(),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  @override
-  HomeState createState() => HomeState();
-}
-
-class HomeState extends State<Home> {
-  List<Item> items = List();
-  Item item;
-  DatabaseReference itemRef;
-
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
-  @override
-  void initState() {
-    super.initState();
-    item = Item("", "");
-    final FirebaseDatabase database = FirebaseDatabase
-        .instance; //Rather then just writing FirebaseDatabase(), get the instance.
-    itemRef = database.reference().child('items');
-    itemRef.onChildAdded.listen(_onEntryAdded);
-    itemRef.onChildChanged.listen(_onEntryChanged);
-  }
-
-  _onEntryAdded(Event event) {
-    setState(() {
-      items.add(Item.fromSnapshot(event.snapshot));
-    });
-  }
-
-  _onEntryChanged(Event event) {
-    var old = items.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
-    setState(() {
-      items[items.indexOf(old)] = Item.fromSnapshot(event.snapshot);
-    });
-  }
-
-  void handleSubmit() {
-    final FormState form = formKey.currentState;
-
-    if (form.validate()) {
-      form.save();
-      form.reset();
-      itemRef.push().set(item.toJson());
-    }
-  }
+class AppHome extends StatelessWidget {
+  const AppHome({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('FB example'),
-      ),
-      resizeToAvoidBottomPadding: false,
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 0,
-            child: Center(
-              child: Form(
-                key: formKey,
-                child: Flex(
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    ListTile(
-                      leading: Icon(Icons.info),
-                      title: TextFormField(
-                        initialValue: "",
-                        onSaved: (val) => item.title = val,
-                        validator: (val) => val == "" ? val : null,
-                      ),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.info),
-                      title: TextFormField(
-                        initialValue: '',
-                        onSaved: (val) => item.body = val,
-                        validator: (val) => val == "" ? val : null,
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.send),
-                      onPressed: () {
-                        handleSubmit();
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Flexible(
-            child: FirebaseAnimatedList(
-              query: itemRef,
-              itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                  Animation<double> animation, int index) {
-                return new ListTile(
-                  leading: Icon(Icons.message),
-                  title: Text(items[index].title),
-                  subtitle: Text(items[index].body),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Item {
-  String key;
-  String title;
-  String body;
-
-  Item(this.title, this.body);
-
-  Item.fromSnapshot(DataSnapshot snapshot)
-      : key = snapshot.key,
-        title = snapshot.value["title"],
-        body = snapshot.value["body"];
-
-  toJson() {
-    return {
-      "title": title,
-      "body": body,
-    };
+    return Scaffold();
   }
 }
