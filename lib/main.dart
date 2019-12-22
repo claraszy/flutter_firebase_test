bb0a049787a287ec171f26ad28d9b48807c9635c
Tomas Navas <tomasnavasgutierrez@gmail.com>
2019-12-22 10:54:11 +0100

    Creación de MapScreen como file. .gitignore actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase/model/postit.dart';
+import 'package:firebase/pages/MapScreen.dart';
 import 'package:firebase/subprogramas/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -14,112 +15,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng _center = const LatLng(42.0, 0.0);
-
-  final Set<Marker> _markers = {};
-
-  LatLng _lastMapPosition = _center;
-
-  MapType _currentMapType = MapType.normal;
-
-  void _onMapTypeButtonPressed() {
-    setState(() {
-      _currentMapType = _currentMapType == MapType.normal
-          ? MapType.satellite
-          : MapType.normal;
-    });
-  }
-
-  
-
-  void _onAddMarkerButtonPressed() {
-    setState(() {
-      _markers.add(Marker(
-        // This marker id can be anything that uniquely identifies each marker.
-        markerId: MarkerId(_lastMapPosition.toString()),
-        position: _lastMapPosition,
-        infoWindow: InfoWindow(
-          title: 'Sitio Chulo',
-          snippet: '5 Star Rating',
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      ));
-    });
-  }
-
-  void _onCameraMove(CameraPosition position) {
-    _lastMapPosition = position.target;
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
   @override
   Widget build(BuildContext context) {
-    final db = Firestore.instance;
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Maps Sample App'),
-            backgroundColor: Colors.green[700],
-          ),
-          body: StreamBuilder(
-              stream: db.collection('postit').snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                QuerySnapshot data = snapshot.data;
-                List<DocumentSnapshot> docs = data.documents;
-                List<Postit> lista = loadData(docs);
-                for (var i = 0; i < lista.length; i++) {
-                  print(lista[i].descripcion);
-                }
-                return Stack(
-                  children: <Widget>[
-                    GoogleMap(
-                      onMapCreated: _onMapCreated,
-                      initialCameraPosition: CameraPosition(
-                        target: _center,
-                        zoom: 5.0,
-                      ),
-                      mapType: _currentMapType,
-                      markers: _markers,
-                      onCameraMove: _onCameraMove,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Column(
-                          children: <Widget>[
-                            FloatingActionButton(
-                              heroTag: 'btn1',
-                              onPressed: _onMapTypeButtonPressed,
-                              materialTapTargetSize:
-                                  MaterialTapTargetSize.padded,
-                              backgroundColor: Colors.green,
-                              child: const Icon(Icons.map, size: 36.0),
-                            ),
-                            SizedBox(height: 16.0),
-                            FloatingActionButton(
-                              heroTag: 'btn2',
-                              onPressed: () => _onAddMarkerButtonPressed(),
-                              materialTapTargetSize:
-                                  MaterialTapTargetSize.padded,
-                              backgroundColor: Colors.green,
-                              child: const Icon(Icons.add_location, size: 36.0),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                );
-              })),
+      home: MapScreen(),
     );
   }
 }
