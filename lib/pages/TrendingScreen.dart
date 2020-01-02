9e4ada4c475654005343dcb17a8855cdc4ac4558
Clara <claraszy98@gmail.com>
2020-01-02 16:49:47 +0100

    Creación de la pantalla Tendencias y Perfil

---
+++ lib/pages/TrendingScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TrendingScreen extends StatefulWidget {
+  @override
+  _TrendingScreenState createState() => _TrendingScreenState();
+}
+
+class _TrendingScreenState extends State<TrendingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trending Screen'),
+      ),
+    );
+  }
+}
