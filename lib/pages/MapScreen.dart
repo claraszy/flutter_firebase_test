ef45d43793e4120b72427ebbec8f2fb099f4c84a
Clara <claraszy98@gmail.com>
2020-01-02 15:53:45 +0100

    Favoritos a tendencias

--- lib/pages/MapScreen.dart
+++ lib/pages/MapScreen.dart
@@ -143,7 +143,7 @@ class _MapScreenState extends State<MapScreen> {
             icon: Icon(
               Icons.favorite,
             ),
-            title: Text("FAVORITOS"),
+            title: Text("TENDENCIAS"),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
@@ -266,15 +266,16 @@ Widget _drawer() {
   //Pasar por parametro los TAGS que hayan en este Geohash
   List<String> lista = ['Enano', 'Android', 'Fiesta'];
   return Drawer(
-      elevation: 16.0,
-      child: ListView.builder(
-        itemCount: lista.length,
-        itemBuilder: (context, index) => InkWell(
-          onTap: () {},
-          child: ListTile(
-            title: Text("#" + lista[index]),
-            trailing: Icon(Icons.arrow_forward_ios),
-          ),
+    elevation: 16.0,
+    child: ListView.builder(
+      itemCount: lista.length,
+      itemBuilder: (context, index) => InkWell(
+        onTap: () {},
+        child: ListTile(
+          title: Text("#" + lista[index]),
+          trailing: Icon(Icons.arrow_forward_ios),
         ),
-      ));
+      ),
+    ),
+  );
 }
