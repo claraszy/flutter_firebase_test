87a69359f8ce6562c2c9c0314e9142ba8e0621f4
Clara <claraszy98@gmail.com>
2020-01-11 20:09:41 +0100

    Chips per a les Tags (widget común)

---
+++ lib/widgets/TagRow.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TagRow extends StatefulWidget {
+  const TagRow({
+    Key key,
+    @required this.tags,
+  }) : super(key: key);
+
+  final List<String> tags;
+
+  @override
+  _TagRowState createState() => _TagRowState();
+}
+
+class _TagRowState extends State<TagRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 300,
+      height: 50,
+      child: Center(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.tags.length,
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Chip(
+                backgroundColor: Colors.white,
+                elevation: 5,
+                label: Text(
+                  '# ' + widget.tags[index].toString(),
+                  style: TextStyle(color: Colors.grey),
+                  overflow: TextOverflow.clip,
+                ),
+                deleteButtonTooltipMessage: 'Delete',
+                deleteIcon: Icon(Icons.delete, size: 15, color: Colors.grey,),
+                onDeleted: () {
+                  setState(() {
+                    widget.tags.remove(widget.tags[index]);
+                  });
+                },
+              ),
+
+              //title: Text('#${tags[index]}', style: TextStyle(fontSize: 12),),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
